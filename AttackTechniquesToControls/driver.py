fa0ab2470d8351274dc1db4b4a47e7972f43a5ab
Ernesto <ernesto.ortizrey@student.montana.edu>
2024-09-12 19:18:16 -0600

    Edited driver database connection. Receiveing arguments for connection as environment variables defined in the docker-compose.yml file. For now, must run with docker-compose and must be commented out from the docker-compose.yml file after the collections are inserted to the database.

--- AttackTechniquesToControls/driver.py
+++ AttackTechniquesToControls/driver.py
@@ -1,10 +1,25 @@
 from arango import ArangoClient
 import json, get_json_from_db, insert_control_data, insert_tech_ctrl_edge, insert_tactic_path
 import rm_dup_tac_tech
+import os
+import logging
+
+# set up logging
+logging.basicConfig(level=logging.INFO)
 
 # access to the arango database
-client = ArangoClient()
-db = client.db('BRON', username = 'root', password = 'changeme')
+arango_url = os.getenv('ARANGO_DB_URL', 'http://brondb:8529')
+arango_db_name = os.getenv('ARANGO_DB_NAME', 'BRON')
+arango_username = os.getenv('ARANGO_DB_USERNAME', 'root')
+arango_password = os.getenv('ARANGO_DB_PASSWORD', 'changeme')
+client = ArangoClient(hosts=arango_url)
+logging.info(f"Connecting to ArangoDB at {arango_url} with username {arango_username} and password {arango_password}")
+db = client.db(arango_db_name, username=arango_username, password=arango_password)
+logging.info(f"Connected to ArangoDB database {arango_db_name}")
+        
+
+#client = ArangoClient()
+#db = client.db('BRON', username = 'root', password = 'changeme')
 
 # open the json file
 with open("nist800-53-r5-mappings2.json", 'r') as file:
