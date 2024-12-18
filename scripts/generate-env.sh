9fb5ea12cb548ee0df1f0a65b19435b730e97cd7
ErnestoOrtiz3 <ernesto.ortizrey@student.montana.edu>
2024-12-18 10:22:53 -0700

    Changed the scripts/generate-env.sh to redirect the.env file to the project root directory where the docker-compose.yml file lives.

--- scripts/generate-env.sh
+++ scripts/generate-env.sh
@@ -2,10 +2,10 @@
 
 # Generate environment variables and write them to .env file
 (
-  echo "HOST_VOLUME_PATH=$(pwd)"  # Current working directory
+  echo "HOST_VOLUME_PATH=$(dirname $(pwd))" # Current working directory
   echo "HOST_UID=$(id -u)"        # User ID of the current user
   echo "HOST_GID=$(id -g)"        # Group ID of the current user
-) > .env
+) > $(dirname $(pwd))/.env
 
 # Optional: Print a message indicating success
 echo ".env file has been generated."
